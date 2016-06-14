976bfbbbee53b4997ffcf4fa72328dc22d3f2b8c
Sharjeel Aziz <saziz@salesforce.com>
2016-06-14 13:55:25 -0700

    added a script to get 1 event

--- java/azure-eventhubs-samples/src/main/java/com/microsoft/azure/eventhubs/samples/ReceiveUsingOffset.java
+++ java/azure-eventhubs-samples/src/main/java/com/microsoft/azure/eventhubs/samples/ReceiveUsingOffset.java
@@ -15,67 +15,37 @@ import com.microsoft.azure.servicebus.*;
 
 public class ReceiveUsingOffset
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 			throws ServiceBusException, ExecutionException, InterruptedException, IOException
 	{
-		final String namespaceName = "----ServiceBusNamespaceName-----";
-		final String eventHubName = "----EventHubName-----";
-		final String sasKeyName = "-----SharedAccessSignatureKeyName-----";
-		final String sasKey = "---SharedAccessSignatureKey----";
+		final String namespaceName = "<namespace>";
+		final String eventHubName = "<eventhub>";
+		final String sasKeyName = "<keyname>";
+		final String sasKey = "<key>";
 		ConnectionStringBuilder connStr = new ConnectionStringBuilder(namespaceName, eventHubName, sasKeyName, sasKey);
-		
+
 		EventHubClient ehClient = EventHubClient.createFromConnectionString(connStr.toString()).get();
-		
+
+
 		// receiver
 		String partitionId = "0"; // API to get PartitionIds will be released as part of V0.2
 		PartitionReceiver receiver = ehClient.createEpochReceiver(
-				EventHubClient.DEFAULT_CONSUMER_GROUP_NAME, 
-				partitionId, 
-				PartitionReceiver.START_OF_STREAM,
+				EventHubClient.DEFAULT_CONSUMER_GROUP_NAME,
+				partitionId,
+				"12312",
 				false,
 				1).get();
-		
-		try
-		{
-			Iterable<EventData> receivedEvents = receiver.receive(100).get();
-			
-			while (true)
-			{
-				int batchSize = 0;
-				if (receivedEvents != null)
-				{
-					for(EventData receivedEvent: receivedEvents)
-					{
-						System.out.println(String.format("Message Payload: %s", new String(receivedEvent.getBody(), Charset.defaultCharset())));
-						System.out.println(String.format("Offset: %s, SeqNo: %s, EnqueueTime: %s", 
-								receivedEvent.getSystemProperties().getOffset(), 
-								receivedEvent.getSystemProperties().getSequenceNumber(), 
-								receivedEvent.getSystemProperties().getEnqueuedTime()));
-						batchSize++;
-					}
-				}
-				
-				System.out.println(String.format("ReceivedBatch Size: %s", batchSize));
-				receivedEvents = receiver.receive(100).get();
+
+		try {
+			Iterable<EventData> receivedEvents = receiver.receiveSync(1);
+			for (EventData receivedEvent : receivedEvents) {
+				System.out.println(receivedEvent);
+				System.out.println(new String(receivedEvent.getBody(), Charset.defaultCharset()));
 			}
-		}
-		finally
-		{
-			// this is paramount; max number of concurrent receiver per consumergroup per partition is 5
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
 			receiver.close().get();
 		}
 	}
-
-	/**
-	 * actual application-payload, ex: a telemetry event
-	 */
-	static final class PayloadEvent
-	{
-		PayloadEvent()	{}
-		
-		public String strProperty;
-		public long longProperty;
-		public int intProperty;
-	}
-
 }
